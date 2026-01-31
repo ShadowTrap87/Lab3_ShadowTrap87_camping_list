aae2d564229d74ab2af1d271a9ab39eb96fedf26
Kennett <kaguilarzaldana@student.cscc.edu>
2026-01-30 21:06:36 -0500

    Added a triple-quoted comment

---
+++ Lab3_ShadowTrap87_list.py
@@ -0,0 +1,7 @@
+"""
+Program Name: Camp Packing List
+Author: Kennett Aguilar-Zaldana
+Purpose: To create a list of items for a camping trip, display the total number of items, and to print the list out alphabetically
+Stater Code: None
+Date: 01/30/26
+"""
