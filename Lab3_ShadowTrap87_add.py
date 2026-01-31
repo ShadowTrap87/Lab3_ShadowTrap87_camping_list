57e65476cf36ea94ac4b564d5840d3961004fd69
Kennett <kaguilarzaldana@student.cscc.edu>
2026-01-30 23:54:29 -0500

    Added triple-quoted comment for Lab3_ShadowTrap87_add.py

---
+++ Lab3_ShadowTrap87_add.py
@@ -0,0 +1,6 @@
+"""
+Program Name: Adding Items to Camping List
+Author: Kennett Aguilar-Zaldana
+Purpose: To import the existing item list, add five more items, and to print it out in reverse order alphabetical order.
+Starter Code: Lab3_ShadowTrap87_list.py
+Date: 01/30/26"""
