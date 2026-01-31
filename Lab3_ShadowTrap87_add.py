73b8d08381a766298102d664b520c89ed1827911
Kennett <kaguilarzaldana@student.cscc.edu>
2026-01-31 00:14:30 -0500

    Added five more items

--- Lab3_ShadowTrap87_add.py
+++ Lab3_ShadowTrap87_add.py
@@ -7,3 +7,11 @@ Date: 01/30/26"""
 
 #Import
 from Lab3_ShadowTrap87_list import camping_items
+
+#Adding items to list
+camping_items.append('Flashlight')
+camping_items.append('Cooking Supplies')
+camping_items.append('Extra Clothes')
+camping_items.append('Outdoor Activities')
+camping_items.append('Hygiene Items')
+
