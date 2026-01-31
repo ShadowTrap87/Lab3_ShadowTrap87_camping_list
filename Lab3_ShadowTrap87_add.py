408173bfa3680cb02546865b8c6ee92e1a697a8c
Kennett <kaguilarzaldana@student.cscc.edu>
2026-01-31 00:06:52 -0500

    Imported Lab3_ShadowTrap87_list to Lab3_ShadowTrap87_add

--- Lab3_ShadowTrap87_add.py
+++ Lab3_ShadowTrap87_add.py
@@ -4,3 +4,6 @@ Author: Kennett Aguilar-Zaldana
 Purpose: To import the existing item list, add five more items, and to print it out in reverse order alphabetical order.
 Starter Code: Lab3_ShadowTrap87_list.py
 Date: 01/30/26"""
+
+#Import
+from Lab3_ShadowTrap87_list import camping_items
