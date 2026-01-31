7374b8c661af68505bcf2bf995aac114612f3cbf
Kennett <kaguilarzaldana@student.cscc.edu>
2026-01-30 21:55:03 -0500

    Added the Alphabetically Sorted list

--- Lab3_ShadowTrap87_list.py
+++ Lab3_ShadowTrap87_list.py
@@ -6,7 +6,12 @@ Stater Code: None
 Date: 01/30/26
 """
 #Camping item list
-camping_items = ["Tent", "Sleeping Bag", "Bug Spray", "Sunscreen", "First-Aid Kit", "Water Bottle", "Map", "Lighter", "Lights", "Food"]
+camping_items = ['Tent', 'Sleeping Bag', 'Bug Spray', 'Sunscreen', 'First-Aid Kit', 'Water Bottle', 'Map', 'Lighter', 'Lights', 'Food']
 
 #Item total
 print ("The total number of items is:", len(camping_items))
+
+#Alphabetical list
+print ("Alphabetically Sorted:")
+for item in sorted(camping_items):
+    print (item)
