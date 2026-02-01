0de3f52fc99822bf631841a5dd0ff9ead7088362
Kennett <kaguilarzaldana@student.cscc.edu>
2026-02-01 14:46:31 -0500

    Added triple-quoted comment for Lab3_ShadowTrap87_remove

---
+++ Lab3_ShadowTrap87_remove.py
@@ -0,0 +1,8 @@
+"""
+Program Name: Removing Binoculars from list
+Author: Kennett Aguilar-Zaldana
+Purpose: To remove 'Binoculars' from the camping list and display the final item count
+Starter Code: Lab3_ShadowTrap87_replace.py
+Date: 02/01/26
+"""
+
