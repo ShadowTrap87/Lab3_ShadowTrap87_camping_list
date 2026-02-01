b1c373f8bb368353dfc34a228919abb0bdd8769b
Kennett <kaguilarzaldana@student.cscc.edu>
2026-01-31 22:23:18 -0500

    Added triple-quoted comment for Lab3_ShadowTrap87_replace

---
+++ Lab3_ShadowTrap87_replace.py
@@ -0,0 +1,7 @@
+"""
+Project Name: Replacing Items in Camping List
+Author: Kennett Aguilar-Zaldana
+Purpose: Replace an item from the camping list with 'binoculars' and to show slice notation
+Starter Code: Lab3_ShadowTrap87_add.py
+Date: 01/31/26"""
+
